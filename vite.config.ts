77f768fa8841510e2e797cdfc2b5b511a4237c26
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-26 09:35:14 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import react from "@vitejs/plugin-react";
+
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
