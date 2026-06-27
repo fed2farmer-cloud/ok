eb1ca3667df86fccfbc6eca8ef5044682d5040cd
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-27 05:49:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import path from "path";
 import { fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
-
 import { defineConfig } from "vite";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [react(),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
