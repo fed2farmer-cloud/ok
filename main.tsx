e1353018906c72ff676742c8b15c7d3f714768c6
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-27 04:58:02 -0400

    Add files via upload

--- main.tsx
+++ main.tsx
@@ -1,16 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
-
-const PUBLISHABLE_KEY =
-  "pk_test_Y29uY3JldGUtZG9scGhpbi04Ni5jbGVyay5hY2NvdW50cy5kZXYk";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+    <App />
   </StrictMode>
 );
