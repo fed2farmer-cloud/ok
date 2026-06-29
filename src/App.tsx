d81a7def85f8bbd4669418a58351f0f060be8ff4
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-29 19:43:58 -0400

    Update App.tsx
    check

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
-import Calculator from './components/Calculator'
-import Marketplace from './components/Marketplace'
-import Security from './components/Security'
-import Footer from './components/Footer'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
 
 function App() {
-  const path = window.location.pathname
-
-  if (path === '/login') return <Login />
-  if (path === '/signup') return <Signup />
-
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
       <Nav />
       <Hero />
-      <Calculator />
-      <Marketplace />
-      <Security />
-      <Footer />
     </div>
   )
 }
