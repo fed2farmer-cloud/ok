0ad63a7ab45de1a4b36bf3850b938e159bb2810f
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-29 19:56:33 -0400

    Update App.tsx
    add marketplace

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Calculator from './components/Calculator'
-
+import Marketplace from './components/Marketplace'
 function App() {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
