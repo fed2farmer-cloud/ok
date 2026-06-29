90f74ec67e52c5b41453e203a0feee456b782c28
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-29 19:50:17 -0400

    Update App.tsx
    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Calculator from './components/Calculator'
 
 function App() {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
       <Nav />
       <Hero />
+      <Calculator />
     </div>
   )
 }
