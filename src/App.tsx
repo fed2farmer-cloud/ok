98cd88af37798434fd032e8e27fa485b383d7b5c
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-29 21:35:36 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Nav from './components/Nav'
+
 function App() {
-  return <h1>APP WORKING</h1>
+  return (
+    <>
+      <h1>APP WORKING</h1>
+      <Nav />
+    </>
+  )
 }
 
 export default App
