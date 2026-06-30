445dda1cd39339343784e9f4d204fee078acd8b2
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-30 04:35:30 -0400

    Update App.tsx
    rollb

--- src/App.tsx
+++ src/App.tsx
@@ -104,10 +104,6 @@ function App() {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
       <Nav />
-      <Hero />
-      <Calculator />
-      <Marketplace />
-      <Security />
     </div>
   )
 }
