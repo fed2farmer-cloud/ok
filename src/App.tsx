9fdd1f051973812424c382680cd415cba3898b7b
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-29 20:05:44 -0400

    Update App.tsx
    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import Nav from './components/Nav'
 import Hero from './components/Hero'
 import Calculator from './components/Calculator'
 import Marketplace from './components/Marketplace'
+
 function App() {
   return (
     <div className="min-h-screen bg-white overflow-x-hidden">
       <Nav />
       <Hero />
       <Calculator />
+      <Marketplace />
     </div>
   )
 }
