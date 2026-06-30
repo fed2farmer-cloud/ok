99157ec72b0feb1775e1e4705b14610f16a74570
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-30 05:28:10 -0400

    Update App.tsx
    safety

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,34 @@
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import Calculator from "./components/Calculator";
-import Marketplace from "./components/Marketplace";
-import Security from "./components/Security";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import { useState } from 'react'
+
+import Nav from './components/Nav'
+import Hero from './components/Hero'
+import Calculator from './components/Calculator'
+import Marketplace from './components/Marketplace'
+import Security from './components/Security'
+
+function Login() {
+  return <h1>LOGIN WORKING</h1>
+}
+
+function Signup() {
+  return <h1>SIGNUP WORKING</h1>
+}
 
 function App() {
-  const path = window.location.pathname;
+  const path = window.location.pathname
 
-  // Login page
-  if (path === "/login") {
-    return <Login />;
-  }
+  if (path === '/login') return <Login />
+  if (path === '/signup') return <Signup />
 
-  // Signup page
-  if (path === "/signup") {
-    return <Signup />;
-  }
-
-  // Home page
   return (
     <div className="min-h-screen bg-white">
       <Nav />
-
       <Hero />
-
-      <section id="calculator">
-        <Calculator />
-      </section>
-
-      <section id="invest">
-        <Marketplace />
-      </section>
-
-      <section id="security">
-        <Security />
-      </section>
+      <Calculator />
+      <Marketplace />
+      <Security />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
