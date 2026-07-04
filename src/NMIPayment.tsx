7c6bd78cd793bc0aeb0bdba2df943c6fbda9e864
fed2farmer-cloud <fed2farmer@gmail.com>
2026-07-04 04:20:25 -0400

    Create NMIPayment.tsx

---
+++ src/NMIPayment.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NMIPayment() {
+  return (
+    <div>
+      <h2>NMI Payment</h2>
+      <p>Payment form will go here.</p>
+    </div>
+  );
+}
