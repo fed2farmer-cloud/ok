871efd46b7ff42ed84c29acd6849e032a5538c3d
fed2farmer-cloud <fed2farmer@gmail.com>
2026-07-05 20:25:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import InvestorDashboard from "./pages/InvestorDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+import LoanApplication from "./pages/LoanApplication";
 
-import Dashboard from "./Dashboard";
-import LoanApplication from "./LoanApplication";
-import AdminDashboard from "./AdminDashboard";
-import InvestorDashboard from "./InvestorDashboard";
 import InvestorWallet from "./InvestorWallet";
 import LoanDocuments from "./LoanDocuments";
 import NMIPayment from "./NMIPayment";
@@ -22,17 +22,18 @@ export default function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/borrower-dashboard" element={<Dashboard />} />
         <Route path="/loan-application" element={<LoanApplication />} />
-        <Route path="/loan-documents" element={<LoanDocuments />} />
 
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route path="/loan-documents" element={<LoanDocuments />} />
 
         <Route path="/investor" element={<InvestorDashboard />} />
         <Route path="/investor-dashboard" element={<InvestorDashboard />} />
         <Route path="/investor-wallet" element={<InvestorWallet />} />
 
-        <Route path="/nmi-payment" element={<NMIPayment />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+
         <Route path="/payment" element={<NMIPayment />} />
+        <Route path="/nmi-payment" element={<NMIPayment />} />
       </Routes>
     </BrowserRouter>
   );
