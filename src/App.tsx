9782daa44927939fd2cbb348f5a039d842c08f7d
fed2farmer-cloud <fed2farmer@gmail.com>
2026-07-05 20:21:38 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./Login";
-import Signup from "./Signup";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 import Dashboard from "./Dashboard";
 import LoanApplication from "./LoanApplication";
 import AdminDashboard from "./AdminDashboard";
@@ -15,13 +16,11 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/borrower-dashboard" element={<Dashboard />} />
-
         <Route path="/loan-application" element={<LoanApplication />} />
         <Route path="/loan-documents" element={<LoanDocuments />} />
 
