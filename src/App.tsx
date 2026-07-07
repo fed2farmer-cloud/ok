c1181d372878c5d65b5ac2628069eced18656597
fed2farmer-cloud <fed2farmer@gmail.com>
2026-07-07 05:01:14 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -6,62 +6,36 @@ import Dashboard from "./pages/Dashboard";
 import InvestorDashboard from "./pages/InvestorDashboard";
 import AdminDashboard from "./pages/AdminDashboard";
 import LoanApplication from "./pages/LoanApplication";
-
-import InvestorWallet from "./InvestorWallet";
-import LoanDocuments from "./LoanDocuments";
-import NMIPayment from "./NMIPayment";
-import BitcoinPayment from "./BitcoinPayment";
 import InvestorMarketplace from "./pages/InvestorMarketplace";
 
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import Calculator from "./components/Calculator";
-import Marketplace from "./components/Marketplace";
-import Security from "./components/Security";
-import Footer from "./components/Footer";
-
-function Home() {
-  return (
-    <>
-      <Nav />
-      <Hero />
-      <Calculator />
-      <Marketplace />
-      <Security />
-      <Footer />
-    </>
-  );
-}
+import LoanDocuments from "./LoanDocuments";
+import InvestorWallet from "./InvestorWallet";
+import NMIPayment from "./NMIPayment";
+import BitcoinPayment from "./BitcoinPayment";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="/" element={<Navigate to="/marketplace" replace />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
 
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/borrower-dashboard" element={<Dashboard />} />
-        <Route path="/loan-application" element={<LoanApplication />} />
-        <Route path="/loan-documents" element={<LoanDocuments />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/borrower" element={<Dashboard />} />
+      <Route path="/investor" element={<InvestorDashboard />} />
+      <Route path="/admin" element={<AdminDashboard />} />
 
-        <Route path="/investor" element={<InvestorDashboard />} />
-        <Route path="/investor-dashboard" element={<InvestorDashboard />} />
-        <Route path="/investor-wallet" element={<InvestorWallet />} />
+      <Route path="/loan-application" element={<LoanApplication />} />
+      <Route path="/loan-documents" element={<LoanDocuments />} />
 
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/marketplace" element={<InvestorMarketplace />} />
+      <Route path="/investor-wallet" element={<InvestorWallet />} />
 
-        <Route path="/payment" element={<NMIPayment />} />
-        <Route path="/nmi-payment" element={<NMIPayment />} />
-<Route
-  path="/marketplace"
-  element={<InvestorMarketplace />}
-/>
-        <Route path="/bitcoin-payment" element={<BitcoinPayment />} />
-      </Routes>
-    </BrowserRouter>
+      <Route path="/payment" element={<NMIPayment />} />
+      <Route path="/bitcoin-payment" element={<BitcoinPayment />} />
+
+      <Route path="*" element={<Navigate to="/marketplace" replace />} />
+    </Routes>
   );
 }
