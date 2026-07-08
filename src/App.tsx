611f68a6b5ee0de162a7b331d64296fb57a43c4b
fed2farmer-cloud <fed2farmer@gmail.com>
2026-07-08 04:20:16 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -16,7 +17,7 @@ import BitcoinPayment from "./BitcoinPayment";
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/marketplace" replace />} />
+      <Route path="/" element={<Home />} />
 
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
@@ -35,7 +36,7 @@ export default function App() {
       <Route path="/payment" element={<NMIPayment />} />
       <Route path="/bitcoin-payment" element={<BitcoinPayment />} />
 
-      <Route path="*" element={<Navigate to="/marketplace" replace />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
