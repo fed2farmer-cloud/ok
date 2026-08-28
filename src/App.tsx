512c4fe835344763f08ce09fde7bb2d47b3a1975
fed2farmer-cloud <fed2farmer@gmail.com>
2026-08-28 03:03:48 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -25,8 +25,9 @@ import AdminServicingDashboard from "./pages/AdminServicingDashboard";
 import AdminFundingDisbursement from "./pages/AdminFundingDisbursement";
 import BorrowerRepayments from "./pages/BorrowerRepayments";
 import SecondaryMarket from "./pages/SecondaryMarket";
-import SecondaryLoanDetails from "./pages/SecondaryLoanDetails";
 import InvestmentCertificateDetails from "./pages/InvestmentCertificateDetails";
+import SecondaryLoanDetails from "./pages/SecondaryLoanDetails";
+import SecondaryCertificateCheckout from "./pages/SecondaryCertificateCheckout";
 
 import LoanDocuments from "./LoanDocuments";
 import InvestorWallet from "./InvestorWallet";
@@ -70,7 +71,8 @@ export default function App() {
         <Route path="/admin/funding-disbursement" element={<AdminFundingDisbursement />} />
         <Route path="/repayments" element={<BorrowerRepayments />} />
         <Route path="/secondary-market" element={<SecondaryMarket />} />
-        <Route path="/secondary-loan/:loanNumber" element={<SecondaryLoanDetails />} />
+        <Route path="/secondary-market/loan/:loanNumber" element={<SecondaryLoanDetails />} />
+        <Route path="/secondary-market/purchase/:listingId" element={<SecondaryCertificateCheckout />} />
         <Route path="/investment-certificate/:certificateNumber" element={<InvestmentCertificateDetails />} />
 
         {/* Showcase / marketing pages */}
